3b69044cef98c048ed52c78f20065ba0a6ceba30
redfoxde <fushiyuchen@outlook.com>
2024-06-02 17:43:56 +0800

    test

---
+++ src/DataBaseConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    //数据库连接
+    private static final String url="jdbc:mysql://localhost:3306/";
+    private static final String user="root";
+    private static final String password="fushiyuchen";
+
+    public static Connection getConnection(){
+        Connection connection;
+        try{
+            connection = DriverManager.getConnection(url,user,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+}
---
+++ src/Room.java
@@ -0,0 +1,8 @@
+public class Room {
+    private int room_id;
+    private String room_number;
+    private String room_type;
+    private double price_per_night;
+    private double discount;
+    private String status;
+}
